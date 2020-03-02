7185dae54715d1bbe91c7eb3d7c1fae2582e5330
YeMinThu21200 <ymt.yeminthu02@gmail.com>
2020-03-02 10:49:30 +0630

    First Commit

---
+++ app/src/main/java/com/example/mylayout/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mylayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        //setContentView(R.layout.linear_layout)
+        //setContentView(R.layout.relative_layout)
+        //setContentView(R.layout.constraint_testone)
+        setContentView(R.layout.constraint_testtwo)
+
+
+    }
+}
